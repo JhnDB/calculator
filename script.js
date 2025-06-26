0458ac5693dc32b412e3c18d420f3ee93131fed2
jhndb <jem.baal2006@gmail.com>
2025-06-26 22:01:52 +0800

    Create operation logic

---
+++ script.js
@@ -0,0 +1,36 @@
+function add(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    return x / y;
+}
+
+function operate(x, y, operator) {
+    switch (operator) {
+        case "+":
+            add(x, y);
+            break;
+        case "-":
+            subtract(x, y);
+            break;
+        case "*":
+            multiply(x, y);
+            break;
+        case "/":
+            divide(x, y);
+            break;
+    }
+}
+
+let x = 1;
+let y = 2;
+let operator = "+";
